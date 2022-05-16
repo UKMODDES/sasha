cec52f051f560630a588fd6272c7695b1b079dd1
clayditch <matthew.stanley@uk.thalesgroup.com>
2022-05-16 22:39:22 +0100

    Added function to open arm gripper

--- common/spot_movements.py
+++ common/spot_movements.py
@@ -1,4 +1,9 @@
+from bosdyn.api import arm_surface_contact_pb2
+from bosdyn.api.arm_surface_contact_pb2 import ArmSurfaceContact
+from bosdyn.api.geometry_pb2 import Vec3
+from bosdyn.api.trajectory_pb2 import SE3Trajectory
 from bosdyn.client import Robot
+from bosdyn.client.frame_helpers import ODOM_FRAME_NAME
 from bosdyn.client.robot_command import RobotCommandClient, blocking_stand, RobotCommandBuilder, block_until_arm_arrives
 
 
@@ -27,3 +32,28 @@ def make_robot_unstow_arm(
 
     robot.logger.info("Unstow command issued.")
     block_until_arm_arrives(command_client, unstow_command_id, 3.0)
+
+
+def make_robot_open_gripper(
+        hand_pose_trajectory: SE3Trajectory,
+        press_force_percentage: Vec3
+) -> ArmSurfaceContact.Request:
+    # Open the gripper
+    gripper_cmd_packed = RobotCommandBuilder.claw_gripper_open_fraction_command(0)
+    gripper_command = gripper_cmd_packed.synchronized_command.gripper_command.claw_gripper_command
+
+    cmd = arm_surface_contact_pb2.ArmSurfaceContact.Request(
+        pose_trajectory_in_task=hand_pose_trajectory,
+        root_frame_name=ODOM_FRAME_NAME,
+        press_force_percentage=press_force_percentage,
+        x_axis=arm_surface_contact_pb2.ArmSurfaceContact.Request.AXIS_MODE_POSITION,
+        y_axis=arm_surface_contact_pb2.ArmSurfaceContact.Request.AXIS_MODE_POSITION,
+        z_axis=arm_surface_contact_pb2.ArmSurfaceContact.Request.AXIS_MODE_FORCE,
+        z_admittance=arm_surface_contact_pb2.ArmSurfaceContact.Request.
+            ADMITTANCE_SETTING_LOOSE,
+        # Enable the cross term so that if the arm gets stuck in a rut, it will retract
+        # upwards slightly, preventing excessive lateral forces.
+        xy_to_z_cross_term_admittance=arm_surface_contact_pb2.ArmSurfaceContact.Request.
+            ADMITTANCE_SETTING_VERY_STIFF,
+        gripper_command=gripper_command)
+    return cmd
