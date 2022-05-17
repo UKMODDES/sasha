ba4a5237ecf744990163e23a2b44d51334d1be60
clayditch <matthew.stanley@uk.thalesgroup.com>
2022-05-17 14:24:07 +0100

    Updated gymnastics routine

--- spot_common/spot_functions/gymnastics_routine_spot_function.py
+++ spot_common/spot_functions/gymnastics_routine_spot_function.py
@@ -1,14 +1,10 @@
 import argparse
-import math
 import sys
 from argparse import Namespace
 
 import bosdyn
 import bosdyn.client
 import bosdyn.client.util
-from bosdyn.api.geometry_pb2 import Vec2
-from bosdyn.client.image import ImageClient
-from bosdyn.client.manipulation_api_client import ManipulationApiClient
 
 from spot_common.spot_control.spot_actions.arm import make_robot_unstow_arm, make_robot_pick_up_object_in_pixel
 from spot_common.spot_control.spot_actions.base_unit import make_robot_stand, make_robot_take_stance, make_robot_walk_x, \
@@ -16,8 +12,7 @@ from spot_common.spot_control.spot_actions.base_unit import make_robot_stand, ma
     make_robot_sit, set_robot_to_walk_mode, set_robot_to_crawl_mode, set_robot_to_amble_mode, set_robot_orientation, \
     make_robot_reset_height, set_robot_to_jog_mode, set_robot_to_hop_mode, make_robot_change_height, \
     make_robot_walk_backwards_and_forwards, make_robot_walk_left_and_right, make_robot_turn_clockwise, \
-    make_robot_turn_anticlockwise, make_robot_sit_and_stand_a_number_of_times
-from spot_common.spot_control.spot_actions.camera import make_robot_take_picture
+    make_robot_turn_anticlockwise, make_robot_sit_and_stand_a_number_of_times, make_robot_shimmy_number_of_times
 from spot_common.spot_control.spot_connection import get_connected_robot
 from spot_common.spot_functions.base_spot_function import BaseSpotFunction
 
@@ -26,7 +21,7 @@ class GymnasticsRoutineSpotFunction(BaseSpotFunction):
     def _add_arguments(self, parser: argparse.ArgumentParser):
         pass
 
-    def execute(self, options: Namespace):
+    def dance(self):
         make_robot_stand(self.robot, self.command_client)
         set_robot_to_hop_mode(self.command_client)
         make_robot_walk_backwards_and_forwards(self.robot_state_client, self.command_client, 1, 2)
@@ -41,6 +36,8 @@ class GymnasticsRoutineSpotFunction(BaseSpotFunction):
 
         make_robot_sit_and_stand_a_number_of_times(self.robot, self.command_client, 2)
 
+        make_robot_shimmy_number_of_times(self.command_client, 4, 0.5, 0.5, 0.5, 0.5)
+
         set_robot_orientation(self.command_client, 0.5, 0.5, 0.5, 0.5)
         make_robot_change_height(self.command_client, 0.2)
         make_robot_change_height(self.command_client, 0.8)
@@ -48,6 +45,10 @@ class GymnasticsRoutineSpotFunction(BaseSpotFunction):
         make_robot_change_height(self.command_client, 0.8)
 
         make_robot_unstow_arm(self.robot, self.command_client)
+
+    def execute(self, options: Namespace):
+        for x in range(10):
+            self.dance()
 
     @staticmethod
     def main(argv=None):
